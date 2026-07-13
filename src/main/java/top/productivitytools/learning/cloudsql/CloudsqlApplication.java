d5d050b1d91ee4188450b5bc12d30857cdd1d1e6
Pawel Wujczyk <pwujczyk@gmail.com>
2026-07-13 21:38:10 +0200

    initial commit

---
+++ src/main/java/top/productivitytools/learning/cloudsql/CloudsqlApplication.java
@@ -0,0 +1,13 @@
+package top.productivitytools.learning.cloudsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/productivitytools/learning/cloudsql/CloudsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package top.productivitytools.learning.cloudsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
